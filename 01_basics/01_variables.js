565535c80ebd64cf483b26253232bde4bdf2dcac
Rudra Pratap <73624720+rp2444941@users.noreply.github.com>
2026-02-11 13:24:14 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=144553
+let accountEmail="rp2444941@gmail.com"
+var accountPassword="1234"
+accountCity="Jaiput"
+
+//accountId=2  // not allowed
+
+accountEmail="rudra23@gmail.com"
+accountPassword="12343"
+accountCity="bengaluru"
+let accountState;
+
+console.log(accountId);
+
+/*
+ prefer  not to use var
+ because of issue in block scope and function scope
+
+ */
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
