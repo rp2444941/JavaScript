e812b46b5f627f90f704f7112217b909e75c99cd
Rudra Pratap <1000011653@dit.edu.in>
2026-03-12 10:14:48 +0530

    event loop

--- 02_JS_ Asynchronous_Model/04_Event_Loop.js
+++ 02_JS_ Asynchronous_Model/04_Event_Loop.js
@@ -124,3 +124,72 @@ await sirf async function ke andar execution ko pause karta hai.
 Bahar wala code (caller) wait nahi karta, wo next line chalata rehta hai.
  */
 
+
+
+
+//5. ============================================Callback Queue & Web APIs==========
+
+console.log("A");
+setTimeout(() => console.log("B"), 1000);
+setTimeout(() => console.log("C"), 0);
+console.log("D");
+
+/**Time 0ms:
+   Stack: [log("A")] → Output: A
+   setTimeout(B,1000) → Web API (timer started)
+   setTimeout(C,0)    → Web API (timer started)
+   Stack: [log("D")] → Output: D
+   Stack empty.
+
+Time ~0ms (C's timer expires instantly):
+   Web API sends C's callback to Queue.
+   Queue: [C]
+
+Event Loop sees stack empty → moves C to Stack:
+   Stack: [C] → log("C") → Output: C
+   Stack empty.
+
+Time 1000ms (B's timer expires):
+   Web API sends B's callback to Queue.
+   Queue: [B]
+
+Event Loop moves B to Stack → Output: B.
+
+Final Output: A, D, C, B
+ */
+
+
+//6. ===================setTimeout – Deep Visual
+
+console.log("1");
+
+setTimeout(() => {
+  console.log("2");
+}, 0);
+
+console.log("3");
+
+/**Step 1:
+   Stack: [log("1")]
+   Output: 1
+   Stack empty.
+
+Step 2:
+   Stack: [setTimeout]
+   setTimeout → Web API (0ms timer)
+   Stack empty.
+
+Step 3:
+   Stack: [log("3")]
+   Output: 3
+   Stack empty.
+
+Step 4: (0ms later)
+   Web API: timer complete → callback moves to Queue.
+   Queue: [() => log("2")]
+
+Step 5: Event Loop
+   Stack empty? ✅
+   Queue has task? ✅ → move callback to Stack.
+   Stack: [() => log("2")] → log("2") → Output: 2
+   Stack empty. */
